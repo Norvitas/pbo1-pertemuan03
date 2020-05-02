7b6633a4fa62dc063225a8918ba3c9e7764a713e
Norvitas <noorvitasari7@gmail.com>
2020-05-02 13:02:15 +0800

    If dengan condition true sehingga statement1 jalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,13 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 53000;
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
 
         if (uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
         } else {
-            System.out.println("Uang cukup, berelaan lah");
+            System.out.println("Uang cukup, selamat menikmati");
         }
     }
 }
